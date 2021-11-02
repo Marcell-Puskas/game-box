b9ee8e916f166efac151861080120d5ea18e77f1
Marcell Puskas <puskasmarcell05@gmail.com>
2021-11-02 20:48:13 +0100

    Added threading to tetris

--- Tetris.cs
+++ Tetris.cs
@@ -1,125 +1,144 @@
 using System;
 using System.IO;
+using System.Threading;
 
 namespace GameBox
 {
     class Tetris
     {
+        const int mapx = 10;
+        const int mapy = 40;
+
+        int posx = 0;
+        int posy = 0;
+        Random blockrandom = new Random();
+        Border border = new Border();
+        int current_block;
+        Tetris_data[,] map;
+        bool run = true;
+        bool gameover = false;
+        bool menu = true;
+
+        int[][,] blocks = new int[][,]
+        {
+            new int[,] { 
+                {1, 1, 1, 1}, 
+            },
+            new int[,] { 
+                {1, 0, 0},
+                {1, 1, 1}
+            },
+            new int[,] {
+                {0, 0, 1},
+                {1, 1, 1}
+            },
+            new int[,] {
+                {1, 1,},
+                {1, 1}
+            },
+            new int[,] {
+                {0, 1, 1},
+                {1, 1, 0}
+            },
+            new int[,] {
+                {0, 1, 0},
+                {1, 1, 1}
+            },
+            new int[,] {
+                {1, 1, 0},
+                {0, 1, 1}
+            }
+        };
+
+        ConsoleColor[] block_color = {
+            
+        };
+
         struct Tetris_data
         {
             public int id;
-            public string color;
+            public ConsoleColor color;
         }
+
         public bool Tetris_game()
         {
-            int[][,] blocks = new int[][,]
-            {
-                new int[,] { 
-                    {1, 1, 1, 1}, 
-                },
-                new int[,] { 
-                    {1, 0, 0},
-                    {1, 1, 1}
-                },
-                new int[,] {
-                    {0, 0, 1},
-                    {1, 1, 1}
-                },
-                new int[,] {
-                    {1, 1,},
-                    {1, 1}
-                },
-                new int[,] {
-                    {0, 1, 1},
-                    {1, 1, 0}
-                },
-                new int[,] {
-                    {0, 1, 0},
-                    {1, 1, 1}
-                },
-                new int[,] {
-                    {1, 1, 0},
-                    {0, 1, 1}
-                }
-            };
+            current_block = blockrandom.Next(0, blocks.Length - 1);
 
-            int mapx = 10;
-            int mapy = 40;
-
-            int posx = 0;
-            int posy = 0;
-
-            string border_top_left = "╔";
-            string border_top = "═";
-            string border_top_right = "╗";
-            string border_bottom_left = "╚";
-            string border_bottom = "═";
-            string border_bottom_right = "╝";
-            string border_vertical = "║";
-
-            Random blockrandom = new Random();
-            int current_block = blockrandom.Next(0, blocks.Length - 1);
-            
-            bool run = true;
-            bool gameover = false;
-
-            Tetris_data[,] map = new Tetris_data[mapx, mapy];
-
-            string border_topline = border_top_left;
-            for (int cx = 0; cx <= mapx - 1; cx++)
-            {
-                border_topline += border_top;
-            }
-            border_topline += border_top_right + "\n";
-
-            string border_bottomline = border_bottom_left;
-            for (int cx = 0; cx <= mapx - 1; cx++)
-            {
-                border_bottomline += border_bottom;
-            }
-            border_bottomline += border_bottom_right + "\n";
+            map = new Tetris_data[mapx, mapy];
 
             Console.Clear();
-            while (run && !gameover)
+
+            border.border_print(mapx, mapy);
+
+            Thread key_getThread = new Thread(new ThreadStart(Key_get));
+            key_getThread.Start();
+            Thread logicThread = new Thread(new ThreadStart(Logic));
+            logicThread.Start();
+            while(run);
+            key_getThread.Join();
+            logicThread.Join();
+            return menu;
+        }
+        public void Screen_print()
+        {
+            for (int cy = 0; cy < mapy; cy++)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(border_topline);
-
-                for (int cy = 0; cy < mapy; cy++)
+                Console.SetCursorPosition(1, cy + 1);
+                for (int cx = 0; cx < mapx; cx++)
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write(border_vertical);
-                    for (int cx = 0; cx < mapx; cx++)
+                    if(posx <= cx && cx < posx + blocks[current_block].GetLength(1) && posy <= cy && cy < posy + blocks[current_block].GetLength(0))
                     {
-                        if(posx <= cx && cx < posx + blocks[current_block].GetLength(1) && posy <= cy && cy < posy + blocks[current_block].GetLength(0))
+                        if (blocks[current_block][cy - posy, cx - posx] == 1)
                         {
-                            if (blocks[current_block][cy - posy, cx - posx] == 1)
-                            {
-                                Console.Write("X");
-                            }
-                            else
-                            {
-                                Console.Write(".");
-                            }
+                            Console.Write("X");
                         }
                         else
                         {
-                            Console.Write(" ");
+                            Console.Write(".");
                         }
                     }
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write(border_vertical + "\n");
+                    else
+                    {
+                        Console.Write(" ");
+                    }
                 }
-
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(border_bottomline);
-
-                if(Console.ReadKey().Key == ConsoleKey.Escape)
-                {
-                    run = false;
-                }
             }
-            return false;
+        }
+        public void Key_get()
+        {
+            string key = "";
+            while(run && !gameover)
+            {
+                key = Convert.ToString(Console.ReadKey().Key);
+
+                switch(key)
+                {
+                    case "Escape":
+                    run = false;
+                    menu = false;
+                    break;
+
+                    case "M":
+                    run = false;
+                    menu = true;
+                    break;
+                }
+
+                Screen_print();
+            }
+        }
+        public void Logic()
+        {
+            while(run && !gameover)
+            {
+                posy += 1;
+
+                Screen_print();
+
+                Thread.Sleep(200);
+            }
         }
     }
 }
